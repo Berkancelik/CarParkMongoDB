5db57166383149b58f5183eb7e56c8fdcf34d40d
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-27 01:08:12 +0300

    MongoDB database connected

--- CarParkUser/Controllers/HomeController.cs
+++ CarParkUser/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using CarParkUser.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
+using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -20,6 +22,27 @@ namespace CarParkUser.Controllers
 
         public IActionResult Index()
         {
+            var settings = MongoClientSettings.FromConnectionString("mongodb+srv://Berkancelik:Berkan123@carparkcluster.jp1db.mongodb.net/CarParkDB?retryWrites=true&w=majority");
+            var client = new MongoClient(settings);
+            var database = client.GetDatabase("CarParkDB");
+            var collection = database.GetCollection<Test>("Test");
+
+            var test = new Test()
+            {
+                _Id = ObjectId.GenerateNewId(),
+                NameSurname = "Berkan Çelik",
+                Age = 28,
+                AddressList = new List<Address>() { new Address
+                {
+                    Title="ev adresi",
+                    Description="istanbul 4. numara "
+                },new Address{
+                Title="İş adresi",
+                Description="Boğaz köprüsü" }
+                }
+            };
+            collection.InsertOne(test);
+
             return View();
         }
 
---
+++ CarParkUser/Models/Test.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CarParkUser.Models
+{
+    public class Test
+    {
+        [BsonId]
+        public ObjectId _Id { get; set; }
+        public string NameSurname { get; set; }
+        public int Age { get; set; }
+        public ICollection<Address> AddressList { get; set; }
+
+    }
+    public class Address
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
