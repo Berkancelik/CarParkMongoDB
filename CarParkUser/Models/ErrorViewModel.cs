e1cda702c0b566ea076b29022a2bb584c7abb678
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-27 00:17:36 +0300

    test

--- CarParkUser/Models/ErrorViewModel.cs
+++ CarParkUser/Models/ErrorViewModel.cs
@@ -4,6 +4,7 @@ namespace CarParkUser.Models
 {
     public class ErrorViewModel
     {
+
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
