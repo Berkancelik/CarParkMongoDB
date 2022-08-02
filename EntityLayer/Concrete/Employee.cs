3ff111091cb2cb49a62d4740921ca5b444ac7999
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-08-02 13:49:44 +0300

    Employee class created

---
+++ EntityLayer/Concrete/Employee.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Employee
+    {
+
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string[] Roles { get; set; }
+        public string PhoneNumber{ get; set; }
+        public string WorkPhoneNumber { get; set; }
+
+    }
+}
