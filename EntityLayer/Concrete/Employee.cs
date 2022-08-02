a6f4976468019ce32f7634678cf581809321678c
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-08-02 13:52:45 +0300

    Adress and Employee Contact Model created

--- EntityLayer/Concrete/Employee.cs
+++ EntityLayer/Concrete/Employee.cs
@@ -17,8 +17,8 @@ namespace EntityLayer.Concrete
         public string Email { get; set; }
         public string Password { get; set; }
         public string[] Roles { get; set; }
-        public string PhoneNumber{ get; set; }
-        public string WorkPhoneNumber { get; set; }
+        public EmployeeContact EmployeeContact { get; set; }
+
 
     }
 }
