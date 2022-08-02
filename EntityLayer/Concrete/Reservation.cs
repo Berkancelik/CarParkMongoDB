b167d5e57016d9070793b31f7e3d8b698bc2a456
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-08-02 14:19:05 +0300

    Reservation Class created

---
+++ EntityLayer/Concrete/Reservation.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Reservation
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string Image { get; set; }
+        public string  Plate { get; set; }
+        public DateTime BeginDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal Price { get; set; }
+        public bool IsExit { get; set; }
+        public bool IsPayment { get; set; }
+
+    }
+}
