abc4b7ed55fd4785020f4d164fa77d07ef56b50e
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-08-02 14:09:11 +0300

    CarPark/Translation/WorkingDay class created

---
+++ EntityLayer/Concrete/CarPark.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class CarPark
+    {
+        [BsonId]
+        public ObjectId Id{ get; set; }
+        public string Name { get; set; }
+        public string[] PhoneNumbers { get; set; }
+        public Address Address { get; set; }
+        public string[] Employees { get; set; }
+        public string WebSite { get; set; }
+        public string[] EmailAdresses { get; set; }
+        public ICollection<WorkingDay> WorkingDays { get; set; }
+
+    }
+}
---
+++ EntityLayer/Concrete/Translation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Translation
+    {
+        public string Culture { get; set; }
+        public string Name { get; set; }
+    }
+}
