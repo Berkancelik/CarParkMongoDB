e09cc07db80a27792df995244594a63265345673
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-08-07 14:26:02 +0300

    IRepository created

---
+++ CoreLayer/Repository/Abstract/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using CoreLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLayer.Repository.Abstract
+{
+    public interface IRepository<TEntity> where TEntity : class, new()
+    {
+        GetManyResult<TEntity> ASQueryable();
+        Task<GetManyResult<TEntity>> ASQueryableAsync();
+    }
+}
