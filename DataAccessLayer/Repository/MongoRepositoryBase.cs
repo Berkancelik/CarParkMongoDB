2b4b15b9265fd62babe9745f24783858a9211522
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-08-07 14:28:36 +0300

    MongoRepositoryBase created

---
+++ DataAccessLayer/Repository/MongoRepositoryBase.cs
@@ -0,0 +1,23 @@
+﻿using CoreLayer.Models;
+using CoreLayer.Repository.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repository
+{
+    public class MongoRepositoryBase<TEntity> : IRepository<TEntity> where TEntity : class, new()
+    {
+        public GetManyResult<TEntity> ASQueryable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetManyResult<TEntity>> ASQueryableAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
